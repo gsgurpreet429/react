466849974fd1121c9c4e128e1505b47593783829
Gurpreet Singh <gsgurpreet429@gmail.com>
2021-02-28 16:30:46 +0530

    combining reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,8 @@
+import { COMMENTS } from '../shared/comments';
+
+export const Comments = (state = Comments, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
